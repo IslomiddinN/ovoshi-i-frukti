e7a466177df3832aaaf08c345191d3ef6ff78e6d
ISLOMIDDIN NURALIZODA <qq6584881@gmail.com>
2025-05-18 20:25:31 +0200

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,5 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
+
 $data = json_decode(file_get_contents('php://input'), true);
+
 // Обработка данных
+file_put_contents('register.log', print_r($data, true));
+
 echo json_encode(['success' => true]);
