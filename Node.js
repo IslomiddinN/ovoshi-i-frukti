15bae17d4771d2f58729483b59c8635e9c814771
ISLOMIDDIN NURALIZODA <qq6584881@gmail.com>
2025-05-18 20:26:04 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,5 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Разрешаем CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Обработчик регистрации
 app.post('/php/register', (req, res) => {
-  // Обработка данных регистрации
-  console.log(req.body);
+  console.log('Получены данные:', req.body);
+  // Здесь должна быть ваша логика регистрации
   res.json({ success: true });
+});
+
+app.listen(3000, () => {
+  console.log('Сервер запущен на порту 3000');
 });
